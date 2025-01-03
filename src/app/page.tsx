b3204874dc7f6f348c17a732f774c5e3249b9e8c
shery123pk <madni.kali@gmail.com>
2025-01-03 22:29:24 +0500

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 import Fonts from "@/components/fonts";
 
-import Hero from "@/components/Hero";
+import Hero from "../components/Hero";
 import Products from "./products/page";
 import Top_sell from "./products/sell";
 import Dress from "@/components/dress";
